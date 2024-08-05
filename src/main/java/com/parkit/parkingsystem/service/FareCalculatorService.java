85cb5eeed3d8e5aaf470b473d3c775eb29c269e1
David <davidroussel171113@outlook.fr>
2024-08-05 23:23:16 +0200

    Modification méthode pour réduction 5% quand discount true et ajout méthod basique appelant la deuxième

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -8,7 +8,11 @@ import java.util.Date;
 
 public class FareCalculatorService {
 
-    public void calculateFare(Ticket ticket){
+    public void calculateFare(Ticket ticket) {
+        calculateFare(ticket, false);
+    }
+
+    public void calculateFare(Ticket ticket, boolean discount){
         if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
@@ -31,14 +35,27 @@ public class FareCalculatorService {
                 }
                 default: throw new IllegalArgumentException("Unkown Parking Type");
             }
-        } else {
-            switch (ticket.getParkingSpot().getParkingType()){
+        } else if (!discount) {
+            switch (ticket.getParkingSpot().getParkingType()) {
                 case CAR: {
                     ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
                     break;
                 }
                 case BIKE: {
                     ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+                    break;
+                }
+                default:
+                    throw new IllegalArgumentException("Unkown Parking Type");
+            }
+        } else {
+            switch (ticket.getParkingSpot().getParkingType()){
+                case CAR: {
+                    ticket.setPrice((duration * Fare.CAR_RATE_PER_HOUR) * 0.95);
+                    break;
+                }
+                case BIKE: {
+                    ticket.setPrice((duration * Fare.BIKE_RATE_PER_HOUR) * 0.95);
                     break;
                 }
                 default: throw new IllegalArgumentException("Unkown Parking Type");
