033d30f8b3982e404887e5a853ae0d520b1b07f5
David <davidroussel171113@outlook.fr>
2024-10-02 23:53:29 +0200

    Refonte des méthode et assert pour validation multiple

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,9 +1,11 @@
 package com.parkit.parkingsystem.integration;
 
+import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.ParkingSpot;
 import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
@@ -11,6 +13,8 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Date;
 
 import static org.mockito.Mockito.*;
 
@@ -51,30 +55,42 @@ public class ParkingDataBaseIT {
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processIncomingVehicle();
         Ticket ticket = ticketDAO.getTicket("ABCDEF");
-        boolean attendue = ticket.getParkingSpot().isAvailable();
+        boolean placePrise = ticket.getParkingSpot().isAvailable();
         //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
-        Assertions.assertFalse(attendue);
+        Assertions.assertFalse(placePrise);// Vérification de la place prise en BDD
+        Assertions.assertEquals(1, ticket.getId());// Vérification ticket crée en BDD
     }
 
     @Test
     public void testParkingLotExit() {
-        testParkingACar();
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+        parkingService.processIncomingVehicle();
         parkingService.processExitingVehicle();
         Ticket ticket = ticketDAO.getTicket("ABCDEF");
-        boolean attendue = ticketDAO.updateTicket(ticket);
+        Date ticketOutTime = ticket.getOutTime();
+        double ticketPrice = ticket.getPrice();
+        double prixBDD = ticketDAO.getPriceBDD("ABCDEF");
+        Date timeBDD = ticketDAO.getTimeBDD("ABCDEF");
         //TODO: check that the fare generated and out time are populated correctly in the database
-        Assertions.assertTrue(attendue);
+        Assertions.assertEquals(ticketPrice, prixBDD);// Vérification prix du ticket/prix dans la BDD
+        Assertions.assertEquals(ticketOutTime, timeBDD);// Vérification date de sortie ticket/date de sortie BDD
     }
 
     @Test
     public void testParkingLotExitRecurringUser() {
-        testParkingACar();
+        //Création d'un ticket vierge pour la BDD
+        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR, false);
+        Ticket ticket = new Ticket();
+        ticket.setParkingSpot(parkingSpot);
+        ticket.setVehicleRegNumber("ABCDEF");
+        ticket.setPrice(0);
+        ticket.setInTime(new Date());
+        ticketDAO.saveTicket(ticket);
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processIncomingVehicle();
         parkingService.processExitingVehicle();
-        int ticket = ticketDAO.getNbTicket("ABCDEF");
-        Assertions.assertEquals(2, ticket);
+        int numberTicket = ticketDAO.getNbTicket("ABCDEF");
+        Assertions.assertEquals(2, numberTicket);// Vérification du nombre de ticket "ABCDEF" dans la BDD
     }
 
 }
